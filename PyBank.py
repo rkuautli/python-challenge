ee95f264be9a6a1398576115dab19496179a354a
rkuautli <rkuautli@gmail.com>
2024-11-25 20:34:07 -0600

    Update PyBank.py

--- PyBank.py
+++ PyBank.py
@@ -1,62 +1,61 @@
-import os
+# This is the file where I will keep all my code for the Module_3 assignment
 import csv
+import os
 
-# Path to collect data from the Resources folder
-budget_data_csv = os.path.join("Resources", "budget_data.csv")
+csvpath = os.path.join("Resources", "pybank.csv")
 
-# Initialize variables
-total_months = 0
-net_total = 0
-previous_profit = 0
-profit_changes = []
-months = []
+with open('./resources/pybank.csv', newline='') as csvfile:
+    bankreader = csv.reader(csvfile, delimiter=',')
+    months_processed = []
+    num_months = 0 
+    total = 0
+    previous_profit = 0
+    changes = []
+    grt_p = [0, ""]
+    grt_d = [0, ""]
+    for row in bankreader:
+        # telling loop to ignore header row
+        if num_months == 0:
+            num_months = num_months + 1 
+            continue
+        profit_num = int(row[1])
+        # reomoves duplicte rows if necessary
+        if row[0] not in months_processed:
+            num_months = num_months + 1 
+            months_processed.append(row[0])
+        # changed row to integer to run code!
+        total = total + profit_num
+        # for average of averages!
+        if previous_profit != 0:
+            change = profit_num - previous_profit
+            changes.append(change)
+             
+            if grt_p[0] < change:
+                grt_p[0] = change
+                grt_p[1] = row[0]
+                
+            if grt_d[0] > change:
+                grt_d[0] = change 
+                grt_d[1] = row[0]
+                
+        previous_profit = profit_num  
+    # removing header count   
+    num_months = num_months - 1
+    
+    average_change = sum(changes) / len(changes)
 
-# Read in the CSV file
-with open(budget_data_csv, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    csv_header = next(csvreader)  # Skip header row
+    #built to split and reverse the dfate column to return month then day
+def split_and_reverse(item:str):
+    return "-".join(list(reversed(item.split("-"))))
 
-    # Iterate through the rows in the CSV
-    for row in csvreader:
-        # Calculate total number of months
-        total_months += 1
-        months.append(row[0])  # Collect the months
-        
-        # Calculate net total amount of Profit/Losses
-        net_total += int(row[1])
-        
-        # Calculate change in profit from previous month to current month
-        if total_months > 1:
-            profit_change = int(row[1]) - previous_profit
-            profit_changes.append(profit_change)
-        
-        previous_profit = int(row[1])
-
-# Calculate average change in profit/losses
-average_change = round(sum(profit_changes) / len(profit_changes), 2)
-
-# Find greatest increase and decrease in profits
-greatest_increase = max(profit_changes)
-greatest_decrease = min(profit_changes)
-
-# Find corresponding months for greatest increase and decrease
-increase_month = months[profit_changes.index(greatest_increase)]
-decrease_month = months[profit_changes.index(greatest_decrease)]
-
-# Print results to terminal
-print("Financial Analysis")
+print("Finanical Analysis")
 print("----------------------------")
-print(f"Total Months: {total_months}")
-print(f"Total: ${net_total}")
-print(f"Average Change: ${average_change}")
-print(f"Greatest Increase in Profits: {increase_month} (${greatest_increase})")
-print(f"Greatest Decrease in Profits: {decrease_month} (${greatest_decrease})")
+print("Total Months: {}".format(num_months))
+print("Total: ${}".format(total))
+print("Average Change: ${}".format(round(average_change, 2)))
+print("Greatest Increase in Profits: {} (${})".format(split_and_reverse(grt_p[1]), grt_p[0]))
+print("Greatest Decrease in Profits: {} (${})".format(split_and_reverse(grt_d[1]), grt_d[0]))
 
-# Write results to text file
-output_file = os.path.join("analysis", "financial_analysis.txt")
-with open(output_file, 'w') as file:
-    file.write("Financial Analysis\n")
-    file.write("----------------------------\n")
     file.write(f"Total Months: {total_months}\n")
     file.write(f"Total: ${net_total}\n")
     file.write(f"Average Change: ${average_change}\n")
